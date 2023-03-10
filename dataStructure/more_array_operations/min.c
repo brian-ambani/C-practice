e9b12394f4e6db9c1d744a4caf37999d40c233f8
brian-ambani <brainambani1@gmail.com>
2023-03-10 10:59:06 +0300

    find the min of an array

---
+++ dataStructure/more_array_operations/min.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[] = {5,6,7,8,10,26,16,15,0,2,17,};
+
+	int min;
+	min = arr[0];
+
+	for (int i = 0; i < 11; i++)
+	{
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	printf("%d\n", min);
+}
+
