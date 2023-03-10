618f6f7a7b3c5f8b57c422f14a1d85bac321cec0
brian-ambani <brainambani1@gmail.com>
2023-03-10 11:15:02 +0300

    find the min in an array

--- dataStructure/more_array_operations/min.c
+++ dataStructure/more_array_operations/min.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
-/* using the same logic you can get the max no. in the array*/
+/** using the same logic you can 
+ * get the max no. in the array
+ *
+ *
+ */
+
+int find_min(int arr[], int length);
 
 int main()
 {
-	int arr[] = {5,6,7,8,10,26,16,15,0,2,17,};
+	
+	int arr1[] = {5,6,7,8,10,7,2,17};
+	int arr2[] = {5,8,10,1,2,17};
+	
+	int min1 = find_min(arr1, 8);
+	int min2 = find_min(arr2, 6);
 
+	printf("min1: %d\n", min1);
+	printf("min2: %d\n", min2);
+	
+}
+
+
+int find_min(int arr[], int length)
+{
 	int min;
 	min = arr[0];
 
-	for (int i = 0; i < 11; i++)
+	for (int i = 1; i < length ; i++)
 	{
 		if (arr[i] < min)
 		{
 			min = arr[i];
 		}
 	}
-	printf("%d\n", min);
+	return min;
 }
+
 
