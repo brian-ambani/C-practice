d89a0f1984f1f275157ea9da4ebf6d89572c69a5
brian-ambani <brainambani1@gmail.com>
2023-03-10 11:48:48 +0300

    number of ocurrences of an element in an array

--- dataStructure/more_array_operations/value_ocurrence.c
+++ dataStructure/more_array_operations/value_ocurrence.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
+int ocurrences(int arr[], int length, int to_find);
+
 int main()
 {
-	int arr[] = {4,9,7,6,5,8,3,2,1,5};
+	int arr1[] = {4,9,7,6,5,8,5,2,1,5};
+	int arr2[] = {4,0,7,6,0,8,0,1,0};
+
+	int findarr1_5 = ocurrences(arr1, 10, 5);
+	int findarr2_0 = ocurrences(arr2, 9, 0);
+
+	printf("# of 5s in arr1: %d\n", findarr1_5);
+	printf("# of 0s in arr1: %d\n", findarr2_0);
+}
+
+int ocurrences(int arr[], int length, int to_find)
+{
 	int count = 0;
-	int to_find = 5;
+	for (int i = 0; i < length; i++ )
+		if (arr[i] == to_find)count++;
 
-	for (int i = 0; i < 10; i++ )
-	{
-		if (arr[i] == to_find)
-			count++;
-	}
-
-	printf("# of found: %d\n", count);
+	return (count);
 }
