033ff1db11f268dc11473338041bf256410d1a0a
brian-ambani <brainambani1@gmail.com>
2023-03-13 02:52:06 +0300

    updated array operations

---
+++ dataStructure/pointers_and_array/2_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int a[2][3], i, j;
+
+	printf("Enter the array elements\n");
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	printf("The result array is:\n");
+	{
+		for (i =0; i < 2;i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				printf("%d\t", a[i][j]);
+			}
+		}
+		printf("\n");
+	}
+	return (0);
+}
